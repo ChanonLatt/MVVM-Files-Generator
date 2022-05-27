a02e32ab0ac401123af6ff2324daf66da416a81a
latt_chanon <chanonlatt@gmail.com>
2022-05-27 21:51:06 +0700

    add new files

---
+++ MVVM Files Generator.xctemplate/___FILEBASENAME___/___FILEBASENAME___Model.swift
@@ -0,0 +1,18 @@
+//  ___FILEHEADER___
+
+
+import Foundation
+
+struct ___FILEBASENAMEASIDENTIFIER___ : Codable {
+
+    
+    private enum CodingKeys: String, CodingKey {
+
+    }
+}
+
+// MARK: - Computed Properties
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+
+}
